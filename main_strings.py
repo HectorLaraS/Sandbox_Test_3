170b5901ba588bc8950b7f381d412cbf0e4c3583
hectorlaras <hector.laras18@outlook.com>
2025-09-20 13:25:18 -0600

    added strip func

--- main_strings.py
+++ main_strings.py
@@ -3,3 +3,10 @@ my_input = "Hello World"
 my_input_list = my_input.split()
 for tmp_str in my_input_list:
     print(tmp_str)
+
+
+#strip
+my_input = " Hello World "
+print(my_input)
+my_input_strp = my_input.strip()
+print(my_input_strp)
